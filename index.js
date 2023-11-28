8fd63ca022adcc1b63e4c076145fc45e4d191854
tomppa350 <152295962+tomppa350@users.noreply.github.com>
2023-11-28 17:38:53 +0000

    Create bot

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-console.log("Hello ATR Workshop!");
+import { Client, GatewayIntentBits } from 'discord.js';
+import OpenAI from 'openai';
+
+const client = new Client ({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_KEY
+})
+
+client.login(process.env.DISCORD_KEY);
+
+client.on("messageCreate", async (message) => {
+    console.log(message.content);
+
+    const userInput = message.content;
+    if (!message.author.bot){
+const aiResponse = await openai.chat.completions.create({
+    messages: [{role: "user", content: userInput}],
+    model: "gpt-3.5-turbo"
+}) 
+
+        client.channels.cache.get(message.channelId).send(aiResponse.choices[0].message.content)
+    }
+});
